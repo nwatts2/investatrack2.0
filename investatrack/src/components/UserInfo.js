454dec30a74de8887e7ced946ce0aab35693f7b0
Nuah <nuah@Mac-mini.local>
2023-01-07 18:29:37 -0700

    Added styling for UserInfo element on user page

--- investatrack/src/components/UserInfo.js
+++ investatrack/src/components/UserInfo.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import '../css/UserInfo.css';
-import profilePic from '../images/tempProfilePic.png';
+import profilePic from '../images/coulten.jpg';
 
 const UserInfo  = ({ currentUser, stockTotals, range }) => {
 
@@ -12,19 +12,25 @@ const UserInfo  = ({ currentUser, stockTotals, range }) => {
                     <img src={profilePic}/>
                 </div>
                 <div className='userTitleText'>
-                    <h3>Welcome to Your Portfolio,</h3>
-                    <h2>{currentUser.fname + ' ' + currentUser.lname}</h2>
+                    <hr />
+                    <h2>{currentUser.fname}</h2>
+                    <h2>{currentUser.lname}</h2>
+                    <hr />
+                </div>
+                <div className='userInfoSub'>
+                    <span>Cash:</span>
+                    <h4>{currentUser.cMoney ? currentUser.cMoney.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</h4>
+                    <span>Dollar Change {'(' + range[0] + ')'}:</span>
+                    <h4 className={stockTotals.change ? (stockTotals.change >= 0 ? 'positiveEntry' : 'negativeEntry') : ''}>{stockTotals.change ? (stockTotals.change >= 0 ? '+' : '') + stockTotals.change.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</h4>
+                    <span>Percent Change {'(' + range[0] + ')'}:</span>
+                    <h4>{stockTotals.percent ? (stockTotals.change >= 0 ? '+' : '') + stockTotals.percent.toLocaleString('en-US', {style: 'percent'}) : '0%'}</h4>
                 </div>
             </div>
             <div className='userInfo'>
                 <div className='userInfoHeader'>
-                    <h1>Net Value: {stockTotals.value ? stockTotals.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</h1>
+                    <h1>Portfolio Value: {stockTotals.value ? stockTotals.value.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</h1>
                 </div>
-                <div className='userInfoSub'>
-                    <span>Cash: {currentUser.cMoney ? currentUser.cMoney.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</span>
-                    <span>Dollar Change {'(' + range[0] + ')'}: {stockTotals.change ? (stockTotals.change >= 0 ? '+' : '') + stockTotals.change.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) : '$0.00'}</span>
-                    <span>Percent Change {'(' + range[0] + ')'}: {stockTotals.percent ? (stockTotals.change >= 0 ? '+' : '') + stockTotals.percent.toLocaleString('en-US', {style: 'percent'}) : '0%'}</span>
-                </div>
+                
 
             </div>
         </div>
